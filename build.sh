78ff686c87268353baf100cd88c8583eadfd6c4d
remes2000 <nieruchalskimichal@gmail.com>
2022-03-13 21:45:17 +0100

    fix build script

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@
 cd frontend/amu-financial-summary
 ng build --configuration production --base-href=/app/ --output-path=../dist
 cd ../../
-go install
+env GOOS=freebsd GOARCH=amd64 go install
 rm -rf dist
 mkdir -p dist
-cp ../../../../bin/amu_financial_summary ./dist/
+cp ../../../../bin/freebsd_amd64/amu_financial_summary ./dist/
 mkdir -p dist/frontend
 cp -r ./frontend/dist/** ./dist/frontend
